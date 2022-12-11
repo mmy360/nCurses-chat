53319c06cde0b2f2fc39dcb5760c1868eb9f33b1
Jack <w.xiangyu@qq.com>
2022-12-11 15:46:35 +0800

    modified:   server/MultipleClients.h 	modified:   server/build/CMakeFiles/CMakeOutput.log 	modified:   server/build/CMakeFiles/silver_server.dir/MultipleClients.cpp.o.d 	deleted:    server/build/CMakeFiles/silver_server.dir/compiler_depend.internal 	modified:   server/build/CMakeFiles/silver_server.dir/compiler_depend.make

--- server/MultipleClients.h
+++ server/MultipleClients.h
@@ -14,22 +14,22 @@ Server class definition
 
 class Server {
 public:
-  Server();
-  ~Server();
-  int initServer();
-  void runServer();
-  template<typename T>
-  void updateDictionary(int key, T value, std::map<int, T> *dictionary);
-  bool userFirstMessage(int key); 
-  void getPK(int key, std::string message); 
-  std::string getPK(int key);
-  void eraseMaps(int sock); 
+	Server();
+	~Server();
+	int initServer();
+	void runServer();
+	template<typename T>
+	void updateDictionary(int key, T value, std::map<int, T> *dictionary);
+	bool userFirstMessage(int key); 
+	void getPK(int key, std::string message); 
+	std::string getPK(int key);
+	void eraseMaps(int sock); 
 
 private:
-  int _listening;
-  std::set<int> _loggedUsers; 
-  // Key containing public keys
-  std::map<int, std::string> _userToPK; 
-  std::map<int, bool> _userFirstMessage; 
-  fd_set _master;
+    int _listening;
+    std::set<int> _loggedUsers; 
+    // Key containing public keys
+    std::map<int, std::string> _userToPK; 
+    std::map<int, bool> _userFirstMessage; 
+    fd_set _master;
 };
